fbacdfff4af0c4a8631b6a45bdaacf1dc3cda720
Sreeram2002 <75155661+Sreeram2002@users.noreply.github.com>
2021-12-05 18:50:15 +0530

    Create CallMom.py

---
+++ CallMom.py
@@ -0,0 +1,46 @@
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = require('twilio')(accountSid, authToken);
+
+client.calls
+      .create({
+         url: 'http://demo.twilio.com/docs/voice.xml',
+         to: '+14155551212',
+         from: '+15017122661'
+       })
+      .then(call => console.log(call.sid));
+{
+  "account_sid": "ACaa77ea155c8f85c61940555a67ab9744",
+  "annotation": null,
+  "answered_by": null,
+  "api_version": "2010-04-01",
+  "caller_name": null,
+  "date_created": "Tue, 31 Aug 2010 20:36:28 +0000",
+  "date_updated": "Tue, 31 Aug 2010 20:36:44 +0000",
+  "direction": "inbound",
+  "duration": "15",
+  "end_time": "Tue, 31 Aug 2010 20:36:44 +0000",
+  "forwarded_from": "+14122010763",
+  "from": "+15017122661",
+  "from_formatted": "(501) 712-2661",
+  "group_sid": null,
+  "parent_call_sid": null,
+  "phone_number_sid": "PNXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  "price": "-0.03000",
+  "price_unit": "USD",
+  "sid": "CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  "start_time": "Tue, 31 Aug 2010 20:36:29 +0000",
+  "status": "completed",
+  "subresource_uris": {
+    "notifications": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Calls/CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Notifications.json",
+    "recordings": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Calls/CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Recordings.json",
+    "feedback": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Calls/CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Feedback.json",
+    "feedback_summaries": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Calls/FeedbackSummary.json",
+    "payments": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Calls/CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Payments.json",
+    "events": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Calls/CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Events.json"
+  },
+  "to": "+14155551212",
+  "to_formatted": "(415) 555-1212",
+  "trunk_sid": null,
+  "uri": "/2010-04-01/Accounts/ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Calls/CAXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX.json",
+  "queue_time": "1000"
+}
